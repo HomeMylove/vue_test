dd5d2dff9f38abf5c7ba32365eb0d54421992030
neta <neta520@163.com>
2022-01-10 21:20:52 +0800

    TodoList

---
+++ src/components/MyList.vue
@@ -0,0 +1,49 @@
+<template>
+     <ul class="todo-main">
+         <MyItem v-for="todoObj in todos" 
+		 :key="todoObj.id" 
+		 :todo="todoObj" 
+		 :checkTodo="checkTodo"
+		 :deleteTodo="deleteTodo">
+		 </MyItem>
+    </ul>
+</template>
+
+<script>
+import MyItem from './MyItem'
+
+export default {
+    name:'MyList',
+    components:{MyItem},
+	props:['todos','checkTodo','deleteTodo']
+}
+</script>
+
+
+<style scoped>
+	/*footer*/
+	.todo-footer {
+		height: 40px;
+		line-height: 40px;
+		padding-left: 6px;
+		margin-top: 5px;
+	}
+
+	.todo-footer label {
+		display: inline-block;
+		margin-right: 20px;
+		cursor: pointer;
+	}
+
+	.todo-footer label input {
+		position: relative;
+		top: -1px;
+		vertical-align: middle;
+		margin-right: 5px;
+	}
+
+	.todo-footer button {
+		float: right;
+		margin-top: 5px;
+	}
+</style>
